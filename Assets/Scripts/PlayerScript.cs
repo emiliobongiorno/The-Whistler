098922741604893f6fe794a438c1c3b63c86ea0a
Emilio Bongiorno <emiliobongiorno@Emilios-MacBook-Pro.local>
2023-02-26 16:24:04 -0300

    Add running animation

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,7 +6,9 @@ public class PlayerScript : MonoBehaviour
 {
 
     public Animator anim;
-    public float moveSpeed = 1.0f;
+    public float walkSpeed = 2.0f;
+    public float runSpeed = 4.0f;
+    private float speed = 2.0f;
     private Rigidbody rb;
 
     void Start()
@@ -31,16 +33,28 @@ public class PlayerScript : MonoBehaviour
         if (moveInput == Vector3.zero)
         {
             anim.SetBool("isWalking", false);
+            anim.SetBool("isRunning", false);
         }
         else
         {
-            anim.SetBool("isWalking", true);
+            if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            {
+                speed = runSpeed;
+                anim.SetBool("isRunning", true);
+                anim.SetBool("isWalking", false);
+            }
+            else 
+            {
+                speed = walkSpeed;
+                anim.SetBool("isWalking", true);
+                anim.SetBool("isRunning", false);
+            }
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * 10, 0);
             //transform.rotation = Quaternion.LookRotation(moveInput);
         }
 
-        transform.Translate(moveInput * moveSpeed * Time.deltaTime);
-        //rb.MovePosition(transform.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+        transform.Translate(moveInput * speed * Time.deltaTime);
+        //rb.MovePosition(transform.position + moveInput * speed * Time.fixedDeltaTime);
     }
 
     void OnCollisionEnter(Collision col) 
