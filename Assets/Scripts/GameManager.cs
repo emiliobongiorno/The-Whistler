2d280bf427f401ea09993ca9d827046cef3694cb
Emilio Bongiorno <emiliobongiorno@Emilios-MacBook-Pro.local>
2023-03-01 20:49:52 -0300

    Add hints in a list

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    private int hintsToFind = 5;
+    public int hintsToFind = 5;
+    public GameObject hintPrefab;
+    private List<GameObject> hints = new List<GameObject>();
 
     void Awake()
     {
@@ -21,10 +23,25 @@ public class GameManager : MonoBehaviour
      
     }
 
+    void Start()
+    {
+        for (int i = 0; i < hintsToFind; i++) {
+            Vector3 hintPosition = Random(new Vector3(-10,0,-10), new Vector3(10,0,10));
+            hints.Add(Instantiate(hintPrefab, hintPosition, Quaternion.identity));
+        }
+        
+    }
+
     public void FindHint()
     {
         hintsToFind -= 1;
         Debug.Log(hintsToFind);
     }
+
+
+    private Vector3 Random(Vector3 min, Vector3 max)
+     {
+         return new Vector3(UnityEngine.Random.Range(min.x, max.x), UnityEngine.Random.Range(min.y, max.y), UnityEngine.Random.Range(min.z, max.z));
+     }
 
 }
