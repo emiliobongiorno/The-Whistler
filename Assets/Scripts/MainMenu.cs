01de887de3a818d2532097ebd41f85db6dac86ff
Emilio Bongiorno <emiliobongiorno@Emilios-MacBook-Pro.local>
2023-03-09 14:46:20 -0300

    Add main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit() 
+    {
+        Application.Quit();
+    }
+    
+}
